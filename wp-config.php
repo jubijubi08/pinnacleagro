b895aab90a5a99f1b4e7470ad0cd10d4eae40f79
Md. Zobair <md.zobair.sust@gmail.com>
2020-02-03 20:04:16 +0600

    db config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pinnacleagro' );
+define( 'DB_NAME', 'pinaclea_db_03feb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'pinaclea_zobair' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Scbd@2049' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
