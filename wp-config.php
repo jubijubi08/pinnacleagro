ab5e8f6c6405c678dd47b6cb20785ec3ffe7120e
Md. Zobair <md.zobair.sust@gmail.com>
2020-02-03 20:13:52 +0600

    db config case update

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pinaclea_db_03feb' );
+define( 'DB_NAME', 'pinaclea_db_03Feb' );
 
 /** MySQL database username */
 define( 'DB_USER', 'pinaclea_zobair' );
